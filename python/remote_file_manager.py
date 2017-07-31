5f1dcb77bdb42e645b2646e65fb17b5f04ef895a
echaos <131315c@gmail.com>
2017-07-31 17:13:10 +0000

    Updating...

--- python/remote_file_manager.py
+++ python/remote_file_manager.py
@@ -8,133 +8,96 @@ BUFFER_SIZE = 8192
 
 class RemoteFileManager():
 
-
     def __init__(self):
         self._current_directory = Config.config().valueof('default_directory')
         self._last_directory = '..'+self._current_directory
         os.chdir(self._current_directory)
         self._disk = Disk()
-
         pass
 
     def send_devicelist(self, client_sock):
-        
         try:
             partition_list = self._disk.list_partitions()
-
         except Exception as e:
             client_sock.senderr(e)
-
         else:
             n = len(partition_list)
-
             client_sock.send(str(n))
 
             if n >= 0:
-
                 if client_sock.recv() == 'ok':
                     for name in partition_list:
                         client_sock.send(self._disk.device(name))
 
             else:
-
                 client_sock.send('No device is available now.')
-
-
                     
     def send_basic_partitioninfo(self, client_sock):
-
         try:
             partition_list = self._disk.list_partitions()
-
         except Exception as e:
             client_sock.senderr(e)
-
         else:
-
             n = len(partition_list)
-
             client_sock.send(str(n))
 
             if n >= 0:
-
                 if client_sock.recv() == 'ok':
                     for name in partition_list:
                         info = self._disk.partitioninfo(name)
                         client_sock.send(info.name()+':'+info.size()+':'+info.label()+':'+info.mountpoint())
             else:
-
                 client_sock.send('No partition information is available now.')
 
     def send_filelist(self, client_sock, path):
-
         try:
-
             file_list = os.listdir(path)
-
         except Exception as e:
             client_sock.senderr(e)
-
         else:
-
             n = len(file_list)
-
             client_sock.send(str(n)) #Send file number
             
             if n >= 0:
-
                 if client_sock.recv() == 'ok':
-
                     for filename in file_list:
                         client_sock.send(filename)
             else:
-                client_sock.send('No file available.')
+                client_sock.send('No file available now.')
 
     def send_currentfilelist(self, client_sock):
         self.send_filelist(client_sock, self._current_directory)
 
     def cd(self, client_sock, cmd_list):
-
         try:
-
             path = cmd_list[1]
-            
-            if os.path.isdir(path):
 
+            if os.path.isdir(path):
                     path = os.path.abspath(path) 
                     if path[-1] != '/':
                         path = path+'/'
                         
                     self._last_directory = self._current_directory
                     self._current_directory = path
-
             else:
                 raise Exception(path+' is not a right directory.')
-
 
         except Exception as e:
             client_sock.senderr(e)
 
         else:
             client_sock.send('0')
-            client_sock.send('successful')
+            client_sock.send('successful')            
             
-            
-
     def send_current_directory(self, client_sock):
         client_sock.send(self._current_directory)
 
-
-
     def send_file(self, client_sock, cmd_list):
-
         try:
-
             filepath = cmd_list[1]
 
             if not os.path.isfile(filepath):
                 raise Exception('File path is not right')
-
 
             if not os.path.isabs(filepath):
                 filepath = self._current_directory + filepath
@@ -143,7 +106,7 @@ class RemoteFileManager():
             
             #Add exception handling here
             file_size = os.path.getsize(filepath)
-        
+
         except Exception as e:
             client_sock.senderr(e)
 
@@ -156,11 +119,11 @@ class RemoteFileManager():
                 if buff == '':
                     break
                 client_sock.send(buff)
-        
-    
 
     def current_directory(self):
         return _current_directory
+
+
 
     
 
--- python/server.py
+++ python/server.py
@@ -2,16 +2,17 @@
 import threading
 import os
 import socket
-from multiprocessing import Process
+from multiprocessing import Process, Manager
 from sock import Sock
 from disk import Disk, PartitionInfo
 from remote_file_manager import RemoteFileManager
     
 class ClientThread(threading.Thread):
-    def __init__(self, client_socket, client_addr):
+    def __init__(self, client_socket, client_addr, status):
         threading.Thread.__init__(self)
         self.client_addr = client_addr
         self.client_sock = Sock(client_socket)
+        self.status = status
 
     def parseCommand(self,line):
         if line:
@@ -24,26 +25,37 @@ class ClientThread(threading.Thread):
                 if mutex.acquire():
                     file_manager.send_devicelist(self.client_sock)
                     mutex.release()
+
                 
                 pass
 
-            if cmd_list[0] == 'df':
+            else if cmd_list[0] == 'df':
 
                 if mutex.acquire():
                     file_manager.send_basic_partitioninfo(self.client_sock)
                     mutex.release()
 
-            if cmd_list[0]== 'get':
+            else if cmd_list[0]== 'get':
                 file_manager.send_file(self.client_sock, cmd_list)
 
-            if cmd_list[0]== 'cd':
+            else if cmd_list[0]== 'cd':
                 file_manager.cd(self.client_sock, cmd_list)
 
-            if cmd_list[0] == 'pwd':
+            else if cmd_list[0] == 'pwd':
                 file_manager.send_current_directory(self.client_sock)
 
-            if cmd_list[0] == 'exit':
+            else if cmd_list[0] = 'ls':
+                if len(cmd_list) > 1:
+                    file_manager.send_filelist(self.client_sock, cmd_list[1])
+                else:
+                    file_manager.send_currentfilelist(self.client_sock)
+
+            else if cmd_list[0] == 'exit':
+                self.status.value = False
                 os._exit(0)
+
+            else:
+                pass
 
     def run(self):
         
@@ -55,33 +67,59 @@ class ClientThread(threading.Thread):
         print self.client_addr[0]+':'+str(self.client_addr[1]) + ' disconnected'
         self.client_socket.close()
 
-def start_server():
+def start_server(status):
     print 'Server started.'
-
+    
+    
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Init TCP socket
     server_socket.bind(('',13136)) #Bind address and port
     server_socket.listen(5) #Listen 
 
-    global file_manager
-    global mutex
 
-    file_manager = RemoteFileManager()
-    mutex = threading.Lock()
 
     while True: #Loop
         (client_socket, address) = server_socket.accept() #When server accept a client
         print address
-        ctthread = ClientThread(client_socket, address)   #Init a thread
+        ctthread = ClientThread(client_socket, address, status)   #Init a thread
         ctthread.start() #Start a thread
 
-def main():
+def broadcast(status):
+        msg = socket.gethostbyname(socket.gethostname())
+        print msg
+        destination = ('233.233.233.233',13135)
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # s.bind(('',0))
+        # s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 20)
 
-    broadcast = Process(target=Sock.broadcast)
-    server = Process(target=start_server)
-    broadcast.start()
+        while True:
+            
+            if status.value == False:
+                print "Running status:"+str(status.value)
+                break
+
+            s.sendto(msg, destination)
+#             (buf, address) = s.recvfrom(10100)
+            # if not len(buff):
+                # break
+
+
+def main():
+    global file_manager
+    global mutex
+    global is_running
+    manager = Manager()
+
+    file_manager = RemoteFileManager() 
+    mutex = threading.Lock()                #Lock
+    is_running = manager.Value('b', True)   #Shared process running state
+    
+    server_broadcast = Process(target=broadcast, args=(is_running,))
+    server = Process(target=start_server, args=(is_running,))
+    server_broadcast.start()
     server.start()
 
-    broadcast.join()
+    server_broadcast.join()
     server.join()
 
     
